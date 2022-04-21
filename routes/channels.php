4496198634dc5d602199140d5c66c15abe81147c
bootleg-boy <kalpesh.94@hotmail.com>
2022-04-21 08:57:09 +0100

    changes in routing

--- routes/channels.php
+++ routes/channels.php
@@ -3,14 +3,7 @@
 use Illuminate\Support\Facades\Broadcast;
 
 /*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
+Some changes
 */
 
 Broadcast::channel('App.User.{id}', function ($user, $id) {
